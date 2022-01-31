8b6dc804a7a75c232c3150c8a24e6b4854c631d8
georgesdsk <sadachok99@gmail.com>
2022-01-31 14:34:49 +0100

    gsdgsdgsdg

--- DI.PruebaAnimaciones/MainPage.xaml.cs
+++ DI.PruebaAnimaciones/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
@@ -18,20 +19,71 @@ using Windows.UI.Xaml.Navigation;
 namespace DI.PruebaAnimaciones
 {
     /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// Animacion para cada uno de los 5 botones. ya que tienen colores diferentes. Debe de aparecer justo en la posicion 
     /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
-            miStoryboard.Begin();
+            // miStoryboard.Begin();
+            crearAnimacion();
+
+            
+        }
+        /// <summary>
+        /// Metodo unificado para crear animacion
+        /// </summary>
+        private void crearAnimacion()
+        {
+            Storyboard str = new Storyboard();
+            DoubleAnimation dbAnimationEjeX = new DoubleAnimation();// tamanio x
+            DoubleAnimation dbAnimationEjeY = new DoubleAnimation();
+            ScaleTransition scale = new ScaleTransfor();
+            dbAnimationEjeX.AutoReverse = true;
+            dbAnimationEjeX.RepeatBehavior = RepeatBehavior.Forever;
+            MiRectangulo.Scale = scale;
+            //dbAnimationEjeY.RepeatBehavior = RepeatBehavior.Forever;
+            //dbAnimationEjeY.AutoReverse = true;
+            dbAnimationEjeX.From = 600;
+            dbAnimationEjeX.To = 2000;
+            dbAnimationEjeX.Duration = TimeSpan.FromSeconds(5); // tendra que ser una variable que se cambie que se cambie segun la velocidad de la aplicacion
+            TranslateTransform movedor = new TranslateTransform();            
+            str.Children.Add(dbAnimationEjeX);
+            Storyboard.SetTarget(dbAnimationEjeX, scale);           
+            Storyboard.SetTargetProperty(str, "ScaleX");
+            str.Begin();
             
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
+
+
+
+
+        //private void animarYConfigurarElementos(Random r, Border borde2, DoubleAnimation dbAnimationEjeX, DoubleAnimation dbAnimationEjeY, Storyboard str)
+        //{
+        //    dbAnimationEjeX.AutoReverse = true;
+        //    dbAnimationEjeX.RepeatBehavior = RepeatBehavior.Forever;
+        //    dbAnimationEjeY.AutoReverse = true;
+        //    dbAnimationEjeY.To = r.Next(-1000, 1000);
+        //    dbAnimationEjeY.RepeatBehavior = RepeatBehavior.Forever;
+        //    dbAnimationEjeX.To = r.Next(-1000, 1000);
+        //    TranslateTransform moveTransform = new TranslateTransform();
+        //    moveTransform.X = r.Next(-1000, 1000);
+        //    moveTransform.Y = r.Next(-1000, 1000);
+        //    borde2.RenderTransform = moveTransform;
+        //    dbAnimationEjeX.Duration = new Duration(TimeSpan.FromSeconds(10));
+        //    dbAnimationEjeY.Duration = new Duration(TimeSpan.FromSeconds(10));
+        //    str.Children.Add(dbAnimationEjeX);
+        //    str.Children.Add(dbAnimationEjeY);
+        //    Storyboard.SetTarget(dbAnimationEjeY, moveTransform);
+        //    Storyboard.SetTarget(dbAnimationEjeX, moveTransform);
+        //    Storyboard.SetTargetProperty(dbAnimationEjeY, "Y");
+        //    Storyboard.SetTargetProperty(dbAnimationEjeX, "X");
+        //    RelativePanel.SetAlignHorizontalCenterWithPanel(borde2, true);
+        //    RelativePanel.SetBelow(borde2, stckMenu);
+        //    rltRoot.Children.Add(borde2);
+        //    str.Begin();
+        //}
     }
 }
